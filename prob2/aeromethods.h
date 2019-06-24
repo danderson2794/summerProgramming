7beba754f91cffdf2267f7a4ffca6c0a74aa89e6
Damion Anderson <daander@clemson.edu>
2019-06-24 15:26:59 -0400

    first push.. Haven't finished this programming problem yet.

---
+++ prob2/aeromethods.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  aeromethods.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/24/2019 02:09:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+void printMenu();
+int getUserInput();
